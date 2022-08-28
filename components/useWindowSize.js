af0b2f588c7654b274d0699c9a8695caf60f2dbb
Chris Palmer <write.to.chris.palmer@gmail.com>
2022-08-28 11:27:08 +0100

    added the first commit with window resiz

---
+++ components/useWindowSize.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+export const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    if (typeof window != "undefined") {
+      };
+
+      window.addEventListener("resize", handleResize);
+
+      // Recursion
+      handleResize();
+
+      return () => window.removeEventListener("resize", handleResize);
+    
+  }, []);
+  return windowSize;
+};
