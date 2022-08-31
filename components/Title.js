74257ac6636a92d5d919af640e7cb71320059a5f
Chris Palmer <write.to.chris.palmer@gmail.com>
2022-08-31 16:20:15 +0300

    added the title animation in separate component

---
+++ components/Title.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export const Title = () => {
+  const [text, setText] = useState("");
+  let possible = '-+*/|}{[]~\\":;?/.><=+-_)(*&^%$#@!)}';
+  let logoRandom = "";
+  let logoTitle = "Shylo Bloom";
+  let i = 0;
+
+  useEffect(() => {
+    setTimeout(function () {
+        const generateRandomTitle = (i, logoRandom) => {
+          setTimeout(function () {
+              setText(logoRandom);
+          }, i * 150 );
+        };
+        for (i = 0; i <= logoTitle.length + 1; i++) {
+          logoRandom = logoTitle.substr(0, i);
+          for (var j = i; j <= logoTitle.length; j++) {
+            logoRandom += possible.charAt(
+              Math.floor(Math.random() * possible.length)
+            );
+          }
+          generateRandomTitle(i, logoRandom);
+        }
+    
+    }, 500);
+  }, [logoRandom]);
+
+  return (
+    <div className="title">
+      {text && text}
+    </div>
+  );
+};
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['./img/trees-001.jpg'],
+  },
 }
 
 module.exports = nextConfig
